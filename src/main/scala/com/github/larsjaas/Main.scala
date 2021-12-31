867e081e8b1a404bed3db8cf89c692cf5ee8bfe3
Lars J. Aas <lars.j.aas@gmail.com>
2021-12-31 15:30:03 +0100

    Print 'TicTacToe game'

--- src/main/scala/com/github/larsjaas/Main.scala
+++ src/main/scala/com/github/larsjaas/Main.scala
@@ -3,14 +3,17 @@ package com.github.larsjaas
 import zio._
 import com.github.larsjaas.BuildInfo
 
-object Main extends zio.App {
+import java.io.IOException
+
+object TicTacToe extends zio.App {
+
+    def program: ZIO[Console, IOException, Unit] =
+        Console.putStrLn("TicTacToe game!").orDieWith(_ => new Error("Boom"))
 
     def run(args: List[String]): ZIO[ZEnv, Nothing, ExitCode] = {
-        if (args.headOption.contains("--version")) {
-            println(s"${BuildInfo.name} v${BuildInfo.version}")
-            return ZIO.succeed(ExitCode.success)
-        }
-
-        ZIO.succeed(ExitCode.success)
+        program.foldM(
+            error => Console.putStrLn(s"Execution failed with: ${error}").orDieWith(_ => new Error("Boom")) *> ZIO.succeed(ExitCode.failure),
+            _ => ZIO.succeed(ExitCode.success)
+        )
     }
 }
